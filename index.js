de87959f24e5b3dec34d87c60132a6e961c8599f
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-06 11:39:37 -0700

    Add cors package and allowed origins modue.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+
+const allowedOrigins = require('./origins/origins');
 
 const app = express();
 require('./routes/storeRoutes')(app);
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const storeitems = require('./utilities/storeitems');
 storeitems.initTestItems(120);
---
+++ origins/origins.js
@@ -0,0 +1,3 @@
+module.exports = [ 'http://salt-store-frontend.s3-website.us-east-2.amazonaws.com/',
+  'http://localhost:3000/'
+];
