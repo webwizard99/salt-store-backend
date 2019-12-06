907e9ad1af36d8526ca6509c8b5799c01ea371a0
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-06 14:12:47 -0700

    Change code to use blanket cors module.

--- index.js
+++ index.js
@@ -6,29 +6,31 @@ const allowedOrigins = require('./origins/origins');
 const app = express();
 require('./routes/storeRoutes')(app);
 
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors);
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+// ({
+//   origin: function(origin, callback){
+//     // allow requests with no origin 
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// })
 
-  next();
-}
+// var allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-app.use(allowCrossDomain);
+//   next();
+// }
+
+// app.use(allowCrossDomain);
 
 const storeitems = require('./utilities/storeitems');
 storeitems.initTestItems(120);
