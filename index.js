14c1eefb10117377476ab1f622492d9fb8bf991c
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-09 19:56:49 -0700

    Remove commented cors code.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 
-const allowedOrigins = require('./origins/origins');
 
 const app = express();
 
@@ -9,39 +8,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-// app.all('/', (req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   next();
-// })
-
 require('./routes/storeRoutes')(app);
 
-
-
-// ({
-//   origin: function(origin, callback){
-//     // allow requests with no origin 
-//     // (like mobile apps or curl requests)
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-// })
-
-// var allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-//   next();
-// }
-
-// app.use(allowCrossDomain);
 
 const storeitems = require('./utilities/storeitems');
 storeitems.initTestItems(120);
