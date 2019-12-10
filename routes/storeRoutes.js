8ff68a29213f0c416d1a2e58d0ce9a26b654593d
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-09 18:59:04 -0700

    Attempt response on store route.

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -1,8 +1,12 @@
+const storeitems = require(storeitems);
+
 module.exports = (app) => {
   app.get('/store/items/:page/:count', (req, res) => {
     const page = req.params.page;
     const count = req.params.count;
 
     console.log(page, count);
+    const returnItems = storeitems.getItems(page, count);
+    res.status(200).json(returnItems);
   });
 }
