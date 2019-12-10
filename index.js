f6cbb19723c237dc2737f21d9e71fea3c7660993
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-09 18:42:01 -0700

    Attempt to set cross origin headers for all routes.

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.all('/', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+})
+
 require('./routes/storeRoutes')(app);
 
 
