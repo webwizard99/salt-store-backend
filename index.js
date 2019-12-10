5027634f4399c19d5559e5be573a0315604ab0e9
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-09 19:50:38 -0700

    Use cors module.

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 
-app.all('/', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-})
+// app.all('/', (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// })
 
 require('./routes/storeRoutes')(app);
 
