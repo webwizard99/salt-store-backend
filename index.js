39e96c6c2d846caef60a37dd0246787ee12a4f75
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-23 15:18:45 -0700

    Add test of postgres db inclusion.

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const { Client } = require('pg');
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+
+
 
 require('./routes/storeRoutes')(app);
 
