3d8bcae70d741c4785af088d7d987cb5ad714333
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-23 18:47:16 -0700

    Attempt to long dbClient object for info.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const router = express.Router();
 router.get('/db', async (req, res) => {
   try {
     const dbClient = await client.connect();
+    console.log(dbClient);
     const result = await dbClient.query('SELECT * FROM public.test_table');
     const results = { 'results': (result) ? result.rows : null};
     res.render('/db', results);
