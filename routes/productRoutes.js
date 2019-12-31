ae10269418f288cfd7be97984f06502b92fb9040
Michael Bragg <michael_bragg99@hotmail.com>
2019-12-31 13:52:16 -0700

    Add put and post routes for product router, untested.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -33,9 +33,6 @@ productRouter.get('/:productId', (req, res) => {
 });
 
 productRouter.post('/', (req, res) => {
-  console.log('reached product post route');
-  console.log(req.body);
-  
   let { key, 
     name, 
     cost, 
@@ -48,46 +45,50 @@ productRouter.post('/', (req, res) => {
     width, 
     depth } = req.body;
   
+  // if request lacks API key for adding products, refuse request
   if (key != process.env.products_key) {
     res.status(404).send();
   }
 
   // validate input types
-  if (typeof name !== 'string') {
+  if (name && typeof name !== 'string') {
     name = name.toString();
   }
-  if (typeof cost !== 'number') {
+  if (cost && typeof cost !== 'number') {
     cost = Number.parseFloat(cost).toFixed(2);
   }
-  if (typeof description !== 'string') {
+  if (description && typeof description !== 'string') {
     description = description.toString();
   }
-  if (typeof img_url !== 'string') {
+  if (img_url && typeof img_url !== 'string') {
     img_url = img_url.toString();
   }
-  if (typeof alt_text !== 'string') {
+  if (alt_text && typeof alt_text !== 'string') {
     alt_text = alt_text.toString();
   }
-  if (typeof category_id !== 'number') {
+  if (category_id && typeof category_id !== 'number') {
     category_id = Number.parseInt(category_id);
   }
-  if (typeof weight !== 'number') {
+  if (weight && typeof weight !== 'number') {
     weight = Number.parseFloat(weight);
   }
-  if (typeof height !== 'number') {
+  if (height && typeof height !== 'number') {
     height = Number.parseFloat(height);
   }
-  if (typeof width !== 'number') {
+  if (width && typeof width !== 'number') {
     width = Number.parseFloat(width);
   }
-  if (typeof depth !== 'number') {
+  if (depth && typeof depth !== 'number') {
     depth = Number.parseFloat(depth);
   }
 
+  // reject request if missing a field
   if (!name || !cost || !description || !img_url || ! alt_text ||
       !category_id || !weight || !height || !width || !depth) {
     res.status(400).send();
   } else {
+
+    // create Product in postgreSQL db from request data
     Product.create({
       name,
       cost,
@@ -104,5 +105,108 @@ productRouter.post('/', (req, res) => {
     .catch(err => console.log(err));
   }
 });
+
+// put product
+productRouter.put('/:productId', (req, res) => {
+  
+  // pull id from req object
+  let id = req.id;
+  
+  // pull fields from req body
+  let { key,
+    name, 
+    cost, 
+    description, 
+    img_url, 
+    alt_text, 
+    category_id, 
+    weight, 
+    height, 
+    width, 
+    depth } = req.body;
+  
+  // if request lacks API key for adding products, refuse request
+  if (key != process.env.products_key) {
+    res.status(404).send();
+  }
+
+  // validate input types
+  if (id && typeof id != 'number') {
+    id = Number.parseInt(id);
+  }
+  if (name && typeof name !== 'string') {
+    name = name.toString();
+  }
+  if (cost && typeof cost !== 'number') {
+    cost = Number.parseFloat(cost).toFixed(2);
+  }
+  if (description && typeof description !== 'string') {
+    description = description.toString();
+  }
+  if (img_url && typeof img_url !== 'string') {
+    img_url = img_url.toString();
+  }
+  if (alt_text && typeof alt_text !== 'string') {
+    alt_text = alt_text.toString();
+  }
+  if (category_id && typeof category_id !== 'number') {
+    category_id = Number.parseInt(category_id);
+  }
+  if (weight && typeof weight !== 'number') {
+    weight = Number.parseFloat(weight);
+  }
+  if (height && typeof height !== 'number') {
+    height = Number.parseFloat(height);
+  }
+  if (width && typeof width !== 'number') {
+    width = Number.parseFloat(width);
+  }
+  if (depth && typeof depth !== 'number') {
+    depth = Number.parseFloat(depth);
+  }
+
+  // reject request if missing a field
+  if (!name || !cost || !description || !img_url || ! alt_text ||
+      !category_id || !weight || !height || !width || !depth) {
+    res.status(400).send();
+  } else {
+
+    // create Product in postgreSQL db from request data
+    Product.update({
+      name,
+      cost,
+      description,
+      img_url,
+      alt_text,
+      category_id,
+      weight,
+      height,
+      width,
+      depth
+    }, { where: {
+      id: id
+    }})
+    .then(product => res.status(200).send(product))
+    .catch(err => console.log(err));
+  }
+});
+
+productRouter.delete('/:productId', (req, res) => {
+  // pull id from req
+  let id = req.id;
+
+  // validate input types
+  if (id && typeof id != 'number') {
+    id = Number.parseInt(id);
+  }
+
+  if (!id) {
+    res.status(400).send();
+  } else {
+    Product.destroy({ where: { id: id } })
+      .then(response => res.status(200).send())
+      .catch(err => console.log(err));
+  }
+})
 
 module.exports = productRouter;
