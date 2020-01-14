f539719019565e0388d01d6587e380d0ac73a74e
Michael Bragg <michael_bragg99@hotmail.com>
2020-01-13 20:12:40 -0700

    Use local react files.

---
+++ public/build/precache-manifest.2f2a381f947905defd05c20d6d7e8971.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0d484d0db1bf32e489a69a7c136000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a35fccfe199a4e8429",
+    "url": "/static/css/main.ab2e0d9e.chunk.css"
+  },
+  {
+    "revision": "7534a57f8756f0d4c994",
+    "url": "/static/js/2.32987a14.chunk.js"
+  },
+  {
+    "revision": "c2a35fccfe199a4e8429",
+    "url": "/static/js/main.43b127d3.chunk.js"
+  },
+  {
+    "revision": "a0fd75d2b77fd27b185c",
+    "url": "/static/js/runtime-main.a7bca83f.js"
+  },
+  {
+    "revision": "338648e241047ffa68676fec0e3fbae3",
+    "url": "/static/media/aromatherapy-ayurveda-b-3188.338648e2.jpg"
+  },
+  {
+    "revision": "b05872ab03e98f00abd9ea150321f6cf",
+    "url": "/static/media/beach-salt-water-sand-17727.b05872ab.jpg"
+  },
+  {
+    "revision": "7ce06027ab8fbf420e4163f8ff6b50f4",
+    "url": "/static/media/karla-alexander-lQb35qVvJGk-unsplash.7ce06027.jpg"
+  },
+  {
+    "revision": "4fb364f5d77625434b70496f19480381",
+    "url": "/static/media/sea-salt-in-white-bowls.4fb364f5.jpg"
+  },
+  {
+    "revision": "f8a2c15899ef3c176fc71e9da32520e6",
+    "url": "/static/media/sea-salt-reserve-resort.f8a2c158.jpg"
+  }
+]);
