33efd3128a877f2b408f324c1836d085b9338d57
Michael Bragg <michael_bragg99@hotmail.com>
2020-01-21 09:45:42 -0700

    Add session and passport modules to project in preparation for user management.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./config/database');
 const bodyParser= require('body-parser');
+const session = require('express-session');
+const Passport = require('passport');
 const path = require('path');
 
 // import route to get test database
@@ -25,6 +27,11 @@ db.authenticate()
   .then(() => console.log('Database connected...'))
   .catch(err => console.log('Error: ' + err));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: 'sheM0zz3l3pf3', resave: false, saveUninitialized: false }));
+app.use(Passport.initialize());
+app.use(Passport.session());
 app.use(express.static('public'));
 
 app.use('/db', testRoute);
@@ -33,13 +40,6 @@ app.use('/db', testRoute);
 app.use('/products', productRoutes);
 
 app.use('/', frontendRoutes);
-
-app.get('/hello', (req, res, next) => {
-  res.status(200)
-    .send('Hi there!');
-
-  next();
-});
 
 
 app.listen(PORT);
