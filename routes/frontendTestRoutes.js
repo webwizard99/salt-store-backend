2d766d2b613b5b54af15aad0f6a96e789c8723d8
Michael Bragg <michael_bragg99@hotmail.com>
2020-01-29 16:41:55 -0700

    Resolve conflicts and revert to commit previous to breaking handlebars (hopefully).

--- routes/frontendTestRoutes.js
+++ routes/frontendTestRoutes.js
@@ -2,12 +2,18 @@ const express = require('express');
 const path = require('path');
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 // const csrfProtection = csrf();
 
 const frontendRouter = express.Router();
 
 // frontendRouter.use(csrfProtection);
 
+=======
+
+const frontendRouter = express.Router();
+
+>>>>>>> parent of 0f44b9c... Add handlebars and move react chunk file references to an object in the config folder.
 =======
 
 const frontendRouter = express.Router();
@@ -25,6 +31,7 @@ frontendRouter.use((req, res, next) => {
 
 frontendRouter.get('/', (req, res) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
   // res.status(200).sendFile(path.join(__dirname, '../public', '/build','index.html'));
   const chunk1 = reactScripts.path + reactScripts.chunk1;
   const chunk2 = reactScripts.path + reactScripts.chunk2;
@@ -39,6 +46,9 @@ frontendRouter.get('/', (req, res) => {
   //   css: css,
   //   csrfToken: req.csrfToken() 
   // });
+=======
+  res.status(200).sendFile(path.join(__dirname, '../public', 'index.html'));
+>>>>>>> parent of 0f44b9c... Add handlebars and move react chunk file references to an object in the config folder.
 =======
   res.status(200).sendFile(path.join(__dirname, '../public', 'index.html'));
 >>>>>>> parent of 0f44b9c... Add handlebars and move react chunk file references to an object in the config folder.
