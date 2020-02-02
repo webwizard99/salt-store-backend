430d494c10fd20c088d0e4b40496e5180172ac90
Michael Bragg <michael_bragg99@hotmail.com>
2020-02-02 07:31:59 -0700

    Set default layout to null to see where to start pathing without it.

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ db.authenticate()
   .catch(err => console.log('Error: ' + err));
 
 // view engine setup
-app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs', layoutsDir: __dirname + 'views/layouts' }));
-app.set('views', path.join(__dirname, '/views'));
+app.engine('.hbs', expressHbs({ defaultLayout: null, extname: '.hbs', layoutsDir: __dirname + 'views/layouts' }));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
