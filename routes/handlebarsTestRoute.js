72ee179cfc38d11a8bae1026a1792a35404e3a64
Michael Bragg <michael_bragg99@hotmail.com>
2020-02-02 07:41:49 -0700

    Add console log to test route to test deployment.

--- routes/handlebarsTestRoute.js
+++ routes/handlebarsTestRoute.js
@@ -3,6 +3,7 @@ const express = require('express');
 const handlebarsRouter = express.Router();
 
 handlebarsRouter.get('/', (req, res) => {
+  console.log('in test route...');
   res.render('test/test');
 });
 
