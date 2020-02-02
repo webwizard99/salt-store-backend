5366fc0395e3eaeae66a6b58e35438ee484f2c87
Michael Bragg <michael_bragg99@hotmail.com>
2020-02-02 08:32:59 -0700

    Attempt to render react hbs page from index.hbs in frontend route.

--- routes/frontendTestRoutes.js
+++ routes/frontendTestRoutes.js
@@ -28,7 +28,7 @@ frontendRouter.get('/', (req, res) => {
   
   // res.status(200).send('front end');
   // res.render('/', {});
-  res.render('layouts/layout', 
+  res.render('index', 
   { script1: chunk1,
     script2: chunk2, 
     unpackingScript: reactChunks.unpackingScript,
