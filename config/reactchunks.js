73826c6f2504f1a90f42580db33965f163adec68
Michael Bragg <michael_bragg99@hotmail.com>
2020-02-09 11:32:28 -0700

    Fix typo in reactChunks file.

--- config/reactchunks.js
+++ config/reactchunks.js
@@ -2,6 +2,6 @@ module.exports = {
   path: 'build/static/js/',
   cssPath: 'build/static/css/',
   chunk1: '2.14337450.chunk.js',
-  chunk2: 'main.e54f592b.chunk',
+  chunk2: 'main.e54f592b.chunk.js',
   css: 'main.bc9068f4.chunk.css'
 }
