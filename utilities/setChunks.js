6d28462f4f3808b0745debdfadb99918101887c8
Michael Bragg <michael_bragg99@hotmail.com>
2020-02-17 15:48:14 -0700

    Set react chunk files in reactChunks based on input from fs in setChunks.

--- utilities/setChunks.js
+++ utilities/setChunks.js
@@ -9,5 +9,6 @@ let jsChunks = jsDir.filter((jsFile) => { return jsFile.match(/.*\.(js)/ig);});
 let cssDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'css'));
 let cssChunk = cssDir.filter((cssFile) => { return cssFile.match(/.*\.(css)/ig)});
 
-console.log(jsChunks);
-console.log(cssChunk);
+reactChunks.chunk1 = jsChunks[0];
+reactChunks.chunk2 = jsChunks[2];
+reactChunks.css = cssChunk[0];
